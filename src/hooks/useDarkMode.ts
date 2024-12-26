07da8c9bab06b902dc7e3b269f41eaaeefa6f3c6
Ruyi Lan <rliu2400@gmail.com>
2024-12-26 12:40:00 -0600

    removed obvious stuff:

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -3,22 +3,18 @@ import { useState, useEffect } from "react";
 export default function useDarkMode() {
     const [darkMode, setDarkMode] = useState(true);
 
-    // Function to toggle dark mode
     const toggleDarkMode = () => {
         setDarkMode((prevMode) => !prevMode);
     };
 
-    // Function to turn on dark mode
     const turnOnDarkMode = () => {
         setDarkMode(true);
     };
 
-    // Function to turn off dark mode
     const turnOffDarkMode = () => {
         setDarkMode(false);
     };
 
-    // Effect to add or remove the "dark" class on the root element
     useEffect(() => {
         if (darkMode) {
             document.documentElement.classList.add("dark");
