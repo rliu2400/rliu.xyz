ae7fd6777f8838ec22a1383720e6ade09c860a1d
Ruyi Lan <rliu2400@gmail.com>
2024-12-27 16:46:05 -0600

    new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: 'class',
+    darkMode: "class",
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,13 +10,28 @@ const config: Config = {
     theme: {
         extend: {
             colors: {
+                red: {
+                    DEFAULT: "rgb(240, 18, 28)",
+                    ...require("tailwindcss/colors").red,
+                },
                 background: "var(--background)",
                 foreground: "var(--foreground)",
             },
+
+            animation: {
+                "fade-out-move-up": "fadeOutMoveUp 0.8s ease-in-out forwards",
+            },
+            keyframes: {
+                fadeOutMoveUp: {
+                    "0%": { opacity: "1", transform: "translateY(0)" },
+                    "100%": { opacity: "0", transform: "translateY(-20px)" },
+                },
+            },
+
             fontFamily: {
-                'geist-sans': ['var(--font-geist-sans)', 'sans-serif'],
-                'geist-mono': ['var(--font-geist-mono)', 'monospace'],
-                'fira-code': ['var(--font-fira-code)', 'monospace'], // Add FiraCode here
+                "geist-sans": ["var(--font-geist-sans)", "sans-serif"],
+                "geist-mono": ["var(--font-geist-mono)", "monospace"],
+                "fira-code": ["var(--font-fira-code)", "monospace"], // Add FiraCode here
             },
         },
     },
