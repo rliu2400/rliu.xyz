467d190a3d714c176b5148a5d2ac6954d30eef10
Ruyi Lan <rliu2400@gmail.com>
2025-01-01 13:32:48 -0600

    more achievements

--- src/components/AchievementsCarousel.tsx
+++ src/components/AchievementsCarousel.tsx
@@ -2,11 +2,19 @@ import React, { useState, useRef } from 'react';
 
 const AchievementsCarousel: React.FC = () => {
   const achievements: string[] = [
-    'AIME Qualifier (2023): Scored 8, top ~800 out of ~300,000 American Math Competition contestants.',
-    'USA Computing Olympiad Gold Division.',
-    'Winner, American Fine Arts Festival at Carnegie Hall.',
-    'Facebook Hacker Cup 2021 Round 2 Qualifier.',
-    // Add more achievements as needed
+    '2024 ICPC Mid-Central Regional Competition Silver Medalist - Nov 2024',
+    'ICPC 2025 NAC Qualifier - Nov 2024: Beat 74 teams to place in the top 4 schools in the Mid-Central Regional Contest of the International Collegiate Programming Contest, advancing to the North America Championships. Member of team MREEOWWW (I picked the name).',
+    'Meta Hacker Cup Round 2 Qualifier - 2021, 2023, 2024 - Oct 2024',
+    'American Invitational Mathematics Exam Qualifier - 2022, 2023 - Feb 2023: Highest Score: 8. Placed in the top 10% (~800) of AIME contestants. One of ~8,000 out of ~300,000 nationwide American Math Competition participants selected to take the AIME.',
+    'USA Computing Olympiad Gold Division - Apr 2022',
+    "2022 President's Volunteer Service Award - Jan 2022",
+    'CS:GO HSEL Fall Major Semifinalist, 2019 (3rd-4th/167 teams) - Dec 2019',
+    "American Fine Arts Festival Concert at Carnegie Hall Winner - Winner's recital solo performance at Carnegie Hall.",
+    "First Place - American Protégé International Piano and Strings Competition - Winner's recital solo performance at Carnegie Hall.",
+    "First Place - Grand Prize Virtuoso International Music Competition - Winner's recital solo performance at the Parco Della Musica in Rome, Italy.",
+    'First Place - Music Festival in Honor of Confucius',
+    'Silver - 2021 Quebec International Music Competition',
+    'Third Place - ENKOR International Competition',
   ];
 
   const [scrolling, setScrolling] = useState(false);
