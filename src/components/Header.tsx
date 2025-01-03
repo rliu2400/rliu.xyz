aff184d9179d0667f09086b0b27e65e948a1762d
Ruyi Lan <rliu2400@gmail.com>
2025-01-03 16:55:56 -0600

    made fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
     return showHeader ? (
         <header
             className={cn(
-                "bg-gray-900 py-4 px-8 w-full sticky top-0 z-50 transition-colors duration-300 fade-in",
+                "bg-gray-900 py-4 px-8 w-full fixed top-0 z-50 transition-colors duration-300 fade-in",
                 className,
             )}
         >
@@ -26,7 +26,7 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
                     href="/home"
                     className="header-item text-xl font-bold text-red-600 hover:text-black-400 px-8 transition"
                 >
-                    R
+                    Ryan
                 </Link>
                 <div className="flex space-x-6">
                     <Link
