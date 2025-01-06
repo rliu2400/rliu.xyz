c10aebbf8a233185bfa52496939cf9f937aded87
Ruyi Lan <rliu2400@gmail.com>
2025-01-06 14:35:16 -0600

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,14 +17,14 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
     return showHeader ? (
         <header
             className={cn(
-                "bg-gray-900 py-4 px-8 w-full fixed top-0 z-50 transition-colors duration-300 fade-in",
+                "bg-transparent py-8 px-8 w-full fixed top-0 z-50 transition-colors duration-300 fade-in",
                 className,
             )}
         >
-            <nav className="container mx-auto flex justify-between items-center">
+            <nav className="container mx-auto flex justify-between items-center w-[70%]">
                 <Link
                     href="/home"
-                    className="header-item text-xl font-bold text-red-600 hover:text-black-400 px-8 transition"
+                    className="header-item text-xl font-bold text-red-600 hover:bg-white px-8 transition"
                 >
                     Ryan
                 </Link>
