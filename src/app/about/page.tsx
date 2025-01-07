80725367c54dd3ff80d3529809d859109bb7bef6
Ruyi Lan <rliu2400@gmail.com>
2025-01-06 19:48:08 -0600

    footer

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AchievementsCarousel from "@/components/AchievementsCarousel";
+import Footer from "@/components/Footer";
 
 const AboutPage = () => {
     return (
         <div className="about-page-container max-w-4xl mx-auto p-6 fade-in">
             <section className="introduction mb-8">
-                <h1 className="text-4xl font-bold mb-4">About Me</h1>
+                <h1 className="text-4xl font-bold mb-4 pt-12">About Me</h1>
                 <p className="text-lg leading-relaxed">
                     Hi, I’m Ryan, a CS and Math student at
                     Northwestern University. I have a deep passion for solving
@@ -61,6 +62,8 @@ const AboutPage = () => {
                     Outside of work, I’m a badminton player and competitive programmer.
                 </p>
             </section>
+
+            <Footer />
         </div>
     );
 };
